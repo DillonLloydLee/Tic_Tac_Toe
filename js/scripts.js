ea14cb9470aaf1437b30a6b0d75ef359b7d16ed8
jose torres & Dillon Lloyd Lee <student@epicodus.com>
2015-09-16 09:47:47 -0700

    edit scripts.js.  First Board test pass.

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+// Board prototype :
+
+function Board(squareOne, squareTwo, squareThree, squareFour, squareFive, squareSix, squareSeven, squareEight, squareNine) {
+  this.squareOne = squareOne;
+  this.squareTwo = squareTwo;
+  this.squareThree = squareThree;
+  this.squareFour = squareFour;
+  this.squareFive  = squareFive;
+  this.squareSix = squareSix;
+  this.squareSeven = squareSeven;
+  this.squareEight = squareEight;
+  this.squareNine = squareNine;
+}
+
+
+// PlayerX prototype :
+
+function PlayerX() {
+
+}
+
+
+// PlayerO prototype :
+
+function PlayerO() {
+  
+}
