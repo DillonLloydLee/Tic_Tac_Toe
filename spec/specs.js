f9bda27e30f712b556af15738665bdc6055351ac
jose torres & Dillon Lloyd Lee <student@epicodus.com>
2015-09-16 09:42:48 -0700

    add first Board test.  Fail.

--- spec/specs.js
+++ spec/specs.js
@@ -1,13 +1,28 @@
-// Tests for Ticket prototype :
+// Tests :
 
-describe("Ticket", function() {
+describe("Board", function() {
 
-  it("it creates a new ticket with certain properties.", function() {
-    var testTicket = new Ticket("Rambo", "2015-09-16", "5:30pm", "$10.25");
-    expect(testTicket.title).to.equal("Rambo");
-    expect(testTicket.date).to.equal("2015-09-16");
-    expect(testTicket.time).to.equal("5:30pm");
-    expect(testTicket.price).to.equal("$10.25");
+  it("it creates a Board with specified properties", function() {
+    var testBoard = new Board("Blank", "Blank", "Blank", "Blank", "Blank", "Blank", "Blank", "Blank", "Blank");
+    expect(testBoard.squareOne).to.equal("Blank");
+    expect(testBoard.squareTwo).to.equal("Blank");
+    expect(testBoard.squareThree).to.equal("Blank");
+    expect(testBoard.squareFour).to.equal("Blank");
+    expect(testBoard.squareFive).to.equal("Blank");
+    expect(testBoard.squareSix).to.equal("Blank");
+    expect(testBoard.squareSeven).to.equal("Blank");
+    expect(testBoard.squareEight).to.equal("Blank");
+    expect(testBoard.squareNine).to.equal("Blank");
   });
 
 });
+
+// describe("PlayerX", function() {
+//
+//   it("it creates a new ticket with certain properties.", function() {
+//     var testPlayerX = new PlayerX("Rambo", "2015-09-16", "5:30pm", "$10.25");
+//     expect(testPlayerX.title).to.equal("Rambo");
+//
+//   });
+//
+// });
